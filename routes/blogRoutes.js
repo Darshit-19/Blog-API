deca4849a0a85830379376a6eee9d58e23e49bd4
Darshit Hairiya <hairiyadarshit@gmail.com>
2025-08-23 01:07:06 +0530

    imports fixed

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { createBlog, getBlogs , getBlogById , updateBlog , deleteBlog} = require("../controllers/blogController");
-const authMiddleware = require("../middleware/authMiddleware");
-const upload = require("../middleware/uploadMiddleware");
+const authMiddleware = require("../middleware/auth");
+const upload = require("../middleware/upload");
 
 router.post("/blogs", authMiddleware, upload.single('image') , createBlog); // create a blog
 router.get("/blogs", authMiddleware , getBlogs); // get all blogs
